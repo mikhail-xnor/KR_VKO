d2dc06bb376247e26ff69dc02382f938261453a8
mikhail-xnor <mikhail-xnor@yandex.ru>
2023-04-18 20:25:18 +0300

    IMPR: spro works

--- spro.sh
+++ spro.sh
@@ -24,95 +24,119 @@ declare -A indArray
 declare -A speedArray
 declare -A destroyArray
 
-
-isTargetDestroyed() {
-tInd=0
-while [ "$tInd" -lt "30" ]; do
-
-(( tInd++ ))
-done
-echo 0
+isTargetDestroyed() { # Пропадает ли цель из топ 30, сразу после уничтожения (удаления файла)???
+    isTargetActive=0
+    isDestroyFileExist=0
+    tInd=0
+    while [ "$tInd" -lt "30" ]; do
+        if [ "${indArray[$tInd]}" == "$1" ] && 
+            [ "${speedArray[$tInd]%.*}" -gt "0" ]; then
+            isTargetActive=1
+            return
+        fi
+        ((tInd++))
+    done
+    if [ "$(find $destroyDir $1 2>/dev/null)" ]; then
+        isDestroyFileExist=1
+    fi
+    if [ "$isTargetActive" == "0"  ]; then
+        if [ "$isDestroyFileExist" == "0"  ]; then
+            echo 1
+        else
+            echo 2
+        fi
+        return
+    fi
+    if [ "$isTargetActive" == "1"  ] &&
+        [ "$isDestroyFileExist" == "1"  ]; then
+        echo 3 #Ожидание запуска противоракеты
+        return
+    fi
+    echo 0 #Промах
 }
-
 
 calcSpeed() {
-local X0=${1%,*}
-local Y0=${1#*,}
+    local X0=${1%,*}
+    local Y0=${1#*,}
 
-local X1=${2%,*}
-local Y1=${2#*,}
+    local X1=${2%,*}
+    local Y1=${2#*,}
 
-local speed=$(echo "scale=2; sqrt(($X1-$X0)^2+($Y1-$Y0)^2)" | bc)
-echo $speed
+    local speed=$(echo "scale=2; sqrt(($X1-$X0)^2+($Y1-$Y0)^2)" | bc)
+    echo $speed
 }
 
-while [ 1 ]
-do
-sleep 0.8
-#echo "new iter"
-tmpTarget=$(ls -t $tmpDir 2>/dev/null | head -n 30)
-ind=0
-for i in $tmpTarget
-do
-targetId=${i:12:6}
-indArray[$ind]=$targetId
-targetXY=$(cat $tmpDir$i)
-targetX=${targetXY%,*}
-targetY=${targetXY#*,}
-if [ "${hashArray[$targetId]}" != "" ]; then
-speedArray[$ind]=$(calcSpeed ${hashArray[$targetId]#*;} "${targetX:1},${targetY:1}")
-else
-speedArray[$ind]=0
-fi
-hashArray[$targetId]="${hashArray[$targetId]#*;};${targetX:1},${targetY:1}"
-#echo "Target: ${indArray[$ind]}: ${hashArray[${indArray[$ind]}]} Speed: ${speedArray[$ind]}"
-ind=$(expr $ind + 1)
-done
+while [ 1 ]; do
+    sleep 0.5
+    #echo "new iter"
+    tmpTarget=$(ls -t $tmpDir 2>/dev/null | head -n 30)
+    ind=0
+    for i in $tmpTarget; do
+        targetId=${i:12:6}
+        indArray[$ind]=$targetId
+        targetXY=$(cat $tmpDir$i)
+        targetX=${targetXY%,*}
+        targetY=${targetXY#*,}
+        if [ "${hashArray[$targetId]}" != "" ]; then
+            speedArray[$ind]=$(calcSpeed ${hashArray[$targetId]#*;} "${targetX:1},${targetY:1}")
+        else
+            speedArray[$ind]=0
+        fi
+        hashArray[$targetId]="${hashArray[$targetId]#*;};${targetX:1},${targetY:1}"
+        #echo "Target: ${indArray[$ind]}: ${hashArray[${indArray[$ind]}]} Speed: ${speedArray[$ind]}"
+        ind=$(expr $ind + 1)
+    done
 
-ind=0
-while [ "$ind" -lt "30" ]
-do
-targetXY=${hashArray[${indArray[$ind]}]#*;}
-targetX=${targetXY%,*}
-targetY=${targetXY#*,}
-distToTarget=$(calcSpeed "$targetXY" "$xCenter,$yCenter")
-#echo "Dist: $distToTarget Coords: $targetXY TEST: ${hashArray[${indArray[$ind]}]}"
-for ((i=0; i!=1; i++)); do
-if [ ${speedArray[$ind]%.*} -ge ${minSpeed[$i]} ] &&
-[ ${speedArray[$ind]%.*} -le ${maxSpeed[$i]} ] &&
-[ ${distToTarget%.*} -le $radius ]; then
+    ind=0
+    while [ "$ind" -lt "30" ]; do
 
-if [ "${fixedTargets[${indArray[$ind]}]}" == "" ]; then
-fixedTargets[${indArray[$ind]}]="f"
-echo "Обнаружена цель ID:${indArray[$ind]} с координатами ${targetX} ${targetY} Speed: ${speedArray[$ind]}"
-fi
+        if [ "${destroyArray[$ind]#*;}" == "n" ]; then
+            checkTargetStatus=$(isTargetDestroyed ${destroyArray[$ind]%;*})
+            if [ "$checkTargetStatus" == "0" ]; then
+                fixedTargets[${destroyArray[$ind]%;*}]="f"
+                destroyArray[$ind]="${destroyArray[$ind]%;*};o"
+                echo "Промах по цели ID:${destroyArray[$ind]%;*}"
+            elif [ "$checkTargetStatus" != "3" ]; then
+                destroyArray[$ind]="${destroyArray[$ind]%;*};d"
+                echo "Цель ID:${destroyArray[$ind]%;*} уничтожена!"
+            fi
+        fi
 
-if [ "$rockets" -ge "0" ] &&
-[ "${fixedTargets[${indArray[$ind]}]}" != "a" ]; then
+        targetXY=${hashArray[${indArray[$ind]}]#*;}
+        targetX=${targetXY%,*}
+        targetY=${targetXY#*,}
+        distToTarget=$(calcSpeed "$targetXY" "$xCenter,$yCenter")
+        #echo "Dist: $distToTarget Coords: $targetXY TEST: ${hashArray[${indArray[$ind]}]}"
+        for ((i = 0; i != 1; i++)); do
+            if [ ${speedArray[$ind]%.*} -ge ${minSpeed[$i]} ] &&
+                [ ${speedArray[$ind]%.*} -le ${maxSpeed[$i]} ] &&
+                [ ${distToTarget%.*} -le $radius ]; then
 
-echo "Выстрел по цели ID:${indArray[$ind]}"
-fixedTargets[${indArray[$ind]}]="a"
-j=0
-while [ "${destroyArray[$j]#*;}" == "a" ]; do
-(( j++ ))
-done
-destroyArray[$j]="${indArray[$ind]};a"
-touch "${destroyDir}${indArray[$ind]}"
-(( rockets-- ))
-else
-echo "Боезапас исчерпан"
-fi
+                if [ "${fixedTargets[${indArray[$ind]}]}" == "" ]; then
+                    fixedTargets[${indArray[$ind]}]="f"
+                    echo "Обнаружена цель ID:${indArray[$ind]} с координатами ${targetX} ${targetY} Speed: ${speedArray[$ind]}"
+                fi
 
-fi
-done
+                if [ "$rockets" -ge "0" ] &&
+                    [ "${fixedTargets[${indArray[$ind]}]}" != "a" ]; then
 
-if [ "${destroyArray[$j]#*;}" == "a" ] &&
-[ "$(isTargetDestroyed ${destroyArray[$j]%;*})" == "1" ]; then
-destroyArray[$j]="${destroyArray[$j]%;*};d"
-echo "Цель с ID:${destroyArray[$j]%;*} уничтожена!"
-fi
+                    echo "Выстрел по цели ID:${indArray[$ind]}"
+                    fixedTargets[${indArray[$ind]}]="a"
+                    j=0
+                    while [ "${destroyArray[$j]#*;}" == "n" ]; do
+                        ((j++))
+                    done
+                    destroyArray[$j]="${indArray[$ind]};n"
+                    touch "${destroyDir}${indArray[$ind]}"
+                    ((rockets--))
+                else
+                    echo "Боезапас исчерпан"
+                fi
 
-(( ind++ ))
-done
+            fi
+        done
+
+        ((ind++))
+    done
 
 done
